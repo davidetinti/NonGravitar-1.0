22b2d9fc07a4852d0d8a223aebfa8a99aeb6add6
Paolo Marzolo <pollo.marzo@gmail.com>
2020-05-12 12:27:19 +0200

    removed resizable property on window

--- src/SourceLoader.cpp
+++ src/SourceLoader.cpp
@@ -3,58 +3,60 @@
 using namespace std;
 using namespace sf;
 
-
-SourceLoader::SourceLoader(){
+SourceLoader::SourceLoader()
+{
     length = 1280;
     height = 720;
     time_per_frame = seconds(1.0f / 120.0f); // 120 frames per second
     settings.antialiasingLevel = 8;
-    window.create(VideoMode(length, height), "Non Gravitar", (Style::Titlebar | Style::Close | Style::Resize), settings);
+    window.create(VideoMode(length, height), "Non Gravitar", (Style::Titlebar | Style::Close /*| Style::Resize*/), settings);
     path = "./../resources/";
     loadTextures();
     loadFonts();
 }
 
-
-void SourceLoader::loadTextures(){
-    for (int i = 0; i < sizeof(texture)/sizeof(texture[0]); i++){
+void SourceLoader::loadTextures()
+{
+    for (int i = 0; i < sizeof(texture) / sizeof(texture[0]); i++)
+    {
         texture[i].loadFromFile(path + to_string(i) + ".png");
     }
 }
 
-
-void SourceLoader::loadFonts(){
-    for (int i = 0; i < sizeof(font)/sizeof(font[0]); i++){
+void SourceLoader::loadFonts()
+{
+    for (int i = 0; i < sizeof(font) / sizeof(font[0]); i++)
+    {
         font[i].loadFromFile(path + to_string(i) + ".ttf");
     }
 }
 
-
-int SourceLoader::getHeight(){
+int SourceLoader::getHeight()
+{
     return height;
 }
 
-
-int SourceLoader::getLength(){
+int SourceLoader::getLength()
+{
     return length;
 }
 
-
-Texture *SourceLoader::getTexture(int n){
+Texture *SourceLoader::getTexture(int n)
+{
     return &texture[n];
 }
 
-
-Font *SourceLoader::getFont(int n){
+Font *SourceLoader::getFont(int n)
+{
     return &font[n];
 }
 
-
-RenderWindow *SourceLoader::getWindow(){
+RenderWindow *SourceLoader::getWindow()
+{
     return &window;
 }
 
-
-Time *SourceLoader::getTimePerFrame(){
+Time *SourceLoader::getTimePerFrame()
+{
     return &time_per_frame;
 }
